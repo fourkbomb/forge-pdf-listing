8fa8e6197ac70a94093e4fdb59701a503b87a094
Simon Shields <keepcalm444@gmail.com>
2016-05-17 00:07:58 +1000

    support the fancy new api

--- app.py
+++ app.py
@@ -8,38 +8,55 @@ app = Flask(__name__)
 
 cached_github_files = []
 
+github_data = {}
+
 def request_github_api():
-    r = requests.get('https://api.github.com/repos/AiryShift/ForgeInformaticsNotes/contents?ref=master')
+    r = requests.get('https://raw.githubusercontent.com/AiryShift/ForgeInformaticsNotes/master/info.json')
     if r.status_code != 200:
         return None
-    if not r.headers['content-type'].startswith('application/json'):
+    try:
+        obj = r.json()
+        cachedGithubResps = {}
+        other = []
+        for el in obj:
+            # grab github response
+            d = '/'.join(el['pdf'].split('/')[:-1])
+            name = el['pdf'].split('/')[-1]
+            resp = {}
+            if d in cachedGithubResps:
+                resp = cachedGithubResps[d]
+            else:
+                r = requests.get('https://api.github.com/repos/AiryShift/ForgeInformaticsNotes/contents/' + d + '?ref=master')
+                resp = r.json() # if this fails we're pretty rip anyway so meh
+                cachedGithubResps[d] = resp
+            for f in resp:
+                # find our one
+                if f['name'].lower() == name.lower():
+                    # got it
+                    other.append({
+                        'pretty': el['name'],
+                        'pdflink': f['download_url'],
+                        'texlink': f['download_url'].replace(el['pdf'], el['tex']), # entirely legit
+                        'sizePDF': make_size_pretty(f['size']),
+                        'sizeTex': '4kB' # chosen by fair dice roll. guaranteed to be random
+                    })
+        return other
+    except e as Exception:
+        print("invalid api response received",e)
         return None
-
-    return r.json()
 
 def make_size_pretty(byte):
     suffix = ['B', 'kB', 'MB', 'GB']
     sidx = 0
-    while byte > 10000 and sidx < len(suffix): 
+    while byte > 10000 and sidx < len(suffix):
         byte /= 1000
         sidx += 1
 
-    return str(byte) + suffix[sidx]
+    return str(int(byte)) + suffix[sidx]
 
 @app.route('/')
 def show_index():
-    display = []
-    print(cached_github_files)
-    for fentry in cached_github_files:
-        print(fentry['name'], fentry['name'].endswith('.pdf'))
-        if fentry['name'].endswith('.pdf') and fentry['type'] == 'file':
-            el = {
-                'name': fentry['name'].split('.pdf')[0],
-                'size': make_size_pretty(fentry['size']),
-                'link': fentry['download_url']
-            }
-            display.append(el)
-    return render_template('index.html', files=display)
+    return render_template('index.html', files=cached_github_files)
 
 
 
@@ -49,5 +66,5 @@ if __name__ == '__main__':
     cached_github_files = request_github_api()
     if cached_github_files is None:
         print("WARNING: no cached github files :(")
-    app.run(threaded=True, port=port, host=ip)
+    app.run(debug=True,threaded=True, port=port, host=ip)
 
